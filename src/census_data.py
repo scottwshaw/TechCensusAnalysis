01373f3639b0e32e6d11dd279d49675e19b02e58
Scott Shaw <scottwshaw@gmail.com>
2020-10-21 19:03:51 +1100

    refactored a bit

--- src/census_data.py
+++ src/census_data.py
@@ -1,13 +1,9 @@
-import gspread
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pandas
 import re
 import seaborn as sns
 
-
-chars_to_num = {'1. Strongly disagree':-2, '2':-1, '3. Neither agree not disagree':0, '4':1, '5. Strongly agree':2, '':0}
-defaults_to_num = {'1. Not applied':-2, '2':-1, '3. Partially applied':0, '4':1, '5. Fully applied':2, '':0}
 def synonym(word):
     synonyms = {
         'Gitlab':'GitLab',
@@ -24,65 +20,6 @@ def synonym(word):
         'FireStore':'FireBase'
     }
     return synonyms.get(word,word)
-
-def to_short_labels(long_label):
-    short_labels = {
-        'Timestamp':'time', 
-        'Username':'username', 
-        'Account':'account', 
-        'Team':'team', 
-        'Number of TWers actively developing code':'twers',
-        'Number of non-TW developers actively developing code':'nontwers', 
-        'City':'city', 
-        'What type of engagement is this?':'type',
-        'How technically complex would you say this project is?':'complexity',
-        'How long has this team been together (project duration)':'duration_to_date',
-        'How long do you anticipate this project to last?':'future_duration',
-        ' [We have influence over the technical decisions on our project]':'project_influence',
-        ' [We have influence over technical decisions across the customer organisation]':'org_influence', 
-        ' [We have a clear understanding of the technology direction/strategy for our client, and how what we are doing furthers that direction]':'business_purpose',
-        ' [This is an engagement that I would recommend to a senior TW engineer to further their career]':'good_for_seniors',
-        ' [This is an engagement that I would recommend to a junior TW engineer to learn fundamental skills]':'good_for_juniors',
-        ' [The technology stack used on this engagement is state of the art]':'cutting_edge',
-        ' [We are responsible for running production systems, including on-call support]':'prod_responsible',
-        'Any other comments?':'project_comments',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Trunk-based development]':'trunk_based_dev',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Test-driven development]':'test_driven_dev',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Pair programming]':'pair_programming',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Build security in]':'build_security_in',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Fast automated build]':'fast_build',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Early and frequent deployment]':'early_freq_deploy',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Quality and debt effectively managed]':'debt_managed',
-        'Please select all of the following practices that are followed on your team  (See https://docs.google.com/presentation/d/1-XEAso777d9Y6KJqLmesF5esMk9TFJUzEaDEheyF8hQ/edit?usp=sharing for a deeper explanation of these practices) [Done means production-ready]':'done_means_prod',
-        'How long does it take to go from code commit to code successfully running in production?':'cycle_time',
-        'How often do we deploy code?':'deploy_freq',
-        'What percentage of changes either result in degraded service or subsequently require remediation (e.g., lead to service impairment or outage, require a hotfix, a rollback, a fix-forward, or a patch)?':'failure_rate',
-        'How long does it generally take to restore service when a service incident occurs(e.g. unplanned outage, service impairment)?':'time_to_restore',
-        'Any other comments about practices?':'defaults_comments',
-        'Type of application':'app_type',
-        'Frontend Framework':'frontend_framework',
-        'Version Control':'source_control',
-        'Artefacts':'artefact_repo',
-        'CI/CD':'build_server',
-        'Programming Language':'language',
-        'Storage':'storage',
-        'Persistence':'persistence',
-        'Observability':'observability',
-        'Logging/Monitoring':'logging',
-        'Cloud Platform':'cloud_vendor',
-        'Provisioning and Deployment':'infrastructure_provisioning',
-        'Serverless Stuff':'serverless',
-        'Container hosting':'container_hosting',
-        'Miscellaneous':'misc'}
-    return short_labels[long_label]
-
-def data_from_google_sheet(sheet_url):
-    gc = gspread.oauth()
-    census_data = gc.open_by_url(sheet_url)
-    sheet = census_data.get_worksheet(0)
-    data = sheet.get_all_values()
-    tslvec = np.vectorize(to_short_labels)
-    return pandas.DataFrame(data=data[1:],columns=tslvec(data[0]))
 
 def expand_results_by_weights(results,weightss):
     expanded = []
--- src/make_plots.py
+++ src/make_plots.py
@@ -1,13 +1,14 @@
 import census_data as cd
+import read_sheets as rs
 
 # Q2
 # sheet_url = 'https://docs.google.com/spreadsheets/d/136TnM1O6hNY7kGLgTcmMUwbdJ6UQF1pglQuNbny_pYQ/edit?usp=sharing'
 #
 # Q3
 sheet_url = 'https://docs.google.com/spreadsheets/d/16njXPlGy2u3hlmA4WJFB64ZVeBx5_b3fxfmv9_ViPGc/edit?usp=sharing'
-d = cd.data_from_google_sheet(sheet_url)
-charsf = d.iloc[:,10:17].applymap(lambda x: cd.chars_to_num[x])
-sensible_defaults = d.iloc[:,18:26].applymap(lambda x: cd.defaults_to_num[x])
+d = rs.data_from_google_sheet(sheet_url)
+charsf = d.iloc[:,10:17].applymap(lambda x: rs.chars_to_num[x])
+sensible_defaults = d.iloc[:,18:26].applymap(lambda x: rs.defaults_to_num[x])
 tech = d.iloc[:,32:45].applymap(lambda x: str(x))
 cd.histogram_weighted_tech(tech, d['twers'])
 cd.histogram_unweighted_team_compositions(d.iloc[:,4:6])
