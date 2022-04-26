37780f8622bc8c90b3b0181217af2c9fc3b3b097
Scott Shaw <scottwshaw@gmail.com>
2022-04-27 09:42:40 +1000

    updated to q1 2022

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
-DATA_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1v7WUiYaKiIQ_U2t9f7e-iFApXKX4kXIkpDOyTZd5_FI/edit?usp=sharing'
-DEFAULT_HISTORY_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1iqMRHN2IlPJO4skJ5gjeBrAa7mJSYPm6d0lR24Md05c/edit?usp=sharing'
-CHAR_HISTORY_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1nFycqEOZcMNwVszOcccc7vGqI7twNaYiYmqQB0RMDCI/edit?usp=sharing'
+DATA_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1-H4g8U80lwPhXj2Rd47CvVIdMkgKfsXykdavyN7RzH4/edit?usp=sharing'
+DEFAULT_HISTORY_SHEET_URL= 'https://docs.google.com/spreadsheets/d/15qiTeR55OQvkheGnwABox0tNBIE0qySST7QeFDnh4Z0/edit?usp=sharing'
+CHAR_HISTORY_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1AyPNaoDMy87s2tqRXkrWLwkATxHaarxLlLAoyBbAnK8/edit?usp=sharing'
 CHARACTERISTICS_COLUMNS = slice(12,19)
 SENSIBLE_DEFAULTS_COLUMNS = slice(20,28)
 TECH_STACK_COLUMNS = slice(33,51)
---
+++ src/config_2021q4.py
@@ -0,0 +1,9 @@
+DATA_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1v7WUiYaKiIQ_U2t9f7e-iFApXKX4kXIkpDOyTZd5_FI/edit?usp=sharing'
+DEFAULT_HISTORY_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1iqMRHN2IlPJO4skJ5gjeBrAa7mJSYPm6d0lR24Md05c/edit?usp=sharing'
+CHAR_HISTORY_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1nFycqEOZcMNwVszOcccc7vGqI7twNaYiYmqQB0RMDCI/edit?usp=sharing'
+CHARACTERISTICS_COLUMNS = slice(12,19)
+SENSIBLE_DEFAULTS_COLUMNS = slice(20,28)
+TECH_STACK_COLUMNS = slice(33,51)
+DORA_COLUMNS = slice(28,32)
+# TEAM_COMPOSITION_COLUMNS = [4,7]
+TEAM_COMPOSITION_COLUMNS = slice(4,7)
