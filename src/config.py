151155beb8bc7e233a7c9aa231f3f32b0e8a30a1
Scott Shaw <scottwshaw@gmail.com>
2022-07-26 20:15:00 +1000

    really updated to 2022 Q2 data and brought poetry project up to date

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
-DATA_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1-H4g8U80lwPhXj2Rd47CvVIdMkgKfsXykdavyN7RzH4/edit?usp=sharing'
-DEFAULT_HISTORY_SHEET_URL= 'https://docs.google.com/spreadsheets/d/15qiTeR55OQvkheGnwABox0tNBIE0qySST7QeFDnh4Z0/edit?usp=sharing'
-CHAR_HISTORY_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1AyPNaoDMy87s2tqRXkrWLwkATxHaarxLlLAoyBbAnK8/edit?usp=sharing'
-CHARACTERISTICS_COLUMNS = slice(12,19)
-SENSIBLE_DEFAULTS_COLUMNS = slice(20,28)
-TECH_STACK_COLUMNS = slice(33,53)
-DORA_COLUMNS = slice(28,32)
+DATA_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1_WiChXsNvGrIZr7RPFbnBMB9Qavae6dV-4AfavGNX3w/edit?usp=sharing'
+DEFAULT_HISTORY_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1jMjbXDhiyRCRU5hAyZToVBsWXQwDySukTB6tpJuEiWA/edit?usp=sharing'
+CHAR_HISTORY_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1WzqAQddxhtfXWIIQhjYCojygXX3ohgU7_yZyGIXlHZc/edit?usp=sharing'
+CHARACTERISTICS_COLUMNS = slice(10,17)
+SENSIBLE_DEFAULTS_COLUMNS = slice(18,26)
+TECH_STACK_COLUMNS = slice(31,49)
+DORA_COLUMNS = slice(26,30)
 TEAM_COMPOSITION_COLUMNS = slice(4,8)
---
+++ src/config_2022q1.py
@@ -0,0 +1,9 @@
+DATA_SHEET_URL= 'https://docs.google.com/spreadsheets/d/1-H4g8U80lwPhXj2Rd47CvVIdMkgKfsXykdavyN7RzH4/edit?usp=sharing'
+DEFAULT_HISTORY_SHEET_URL= 'https://docs.google.com/spreadsheets/d/15qiTeR55OQvkheGnwABox0tNBIE0qySST7QeFDnh4Z0/edit?usp=sharing'
+CHAR_HISTORY_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1AyPNaoDMy87s2tqRXkrWLwkATxHaarxLlLAoyBbAnK8/edit?usp=sharing'
+CHARACTERISTICS_COLUMNS = slice(12,19)
+SENSIBLE_DEFAULTS_COLUMNS = slice(20,28)
+TECH_STACK_COLUMNS = slice(33,51)
+DORA_COLUMNS = slice(28,32)
+# TEAM_COMPOSITION_COLUMNS = [4,7]
+TEAM_COMPOSITION_COLUMNS = slice(4,7)
